a4546882f891dcd41058bc644f04c20215557f17
sina <sina.sipamo@gmail.com>
2025-07-19 19:15:21 +0330

    update database and handle creation of a user

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 class Todos(Base):
     __tablename__ = "todos"
@@ -9,3 +23,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     is_complete = Column(Boolean)
+    owner_id = Column(Integer, ForeignKey("users.id"))
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,31 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from starlette import status
 
 router = APIRouter()
 
-@router.get("/auth")
-async def get_user():
-    return {"user": "Authenticated User"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest, status_code=status.HTTP_201_CREATED):
+    """Create a new user in the system. """
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        hashed_password = create_user_request.password,  # In a real app, hash the password
+        role = create_user_request.role,
+        is_active = True
+    )
+    
+    return create_user_model
+
